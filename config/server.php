aade3b80aa85a0850f3175355f89c93ad1f99f41
Okki Setyawan <okki.setyawan@mti.moratelindo.id>
2024-09-20 05:34:13 +0700

    update db

--- config/server.php
+++ config/server.php
@@ -1,8 +1,11 @@
 <?php
 // 1. Connect ke database
-$sqlconn=@mysql_connect("localhost","root","b1m4ntara");
+// $sqlconn=@mysql_connect("localhost","root","b1m4ntara");
+$sqlconn=@mysql_connect("localhost","root","smkBM2024_OKE!");
+// var_dump($sqlconn)
 // 2. Pilih database
 date_default_timezone_set("Asia/Jakarta");
 mysql_select_db("dbpusat2", $sqlconn);
 $mode = "lokal"; // pilih 'lokal' atau 'pusat'
 ?>
+ 
